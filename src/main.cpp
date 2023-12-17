56745bc6b2edcbcc5f8ab1e97817a055b97c4dfd
nicolas <nicolasantiagomez4@gmail.com>
2023-12-17 19:01:43 -0300

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,82 @@
 #include <SFML/Graphics.hpp>
+#include <thread>
 
-int main()
+
+void noFocusThread(sf::Window& window)
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
 
-    while (window.isOpen())
+    window.create({ 400, 300 }, "No Focus", sf::Style::Default);
+
+    while(window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        sf::Event event;
+
+        while(window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if(event.type == sf::Event::Closed)
             {
                 window.close();
             }
         }
 
-        window.clear();
+
         window.display();
     }
+}
+
+
+int main()
+{
+
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+
+    auto window = sf::RenderWindow{ { desktop.width, desktop.height }, "titulo", sf::Style::Default };
+    window.setFramerateLimit(144);
+
+
+
+    bool onetime = true;
+
+    while (window.isOpen())
+    {
+
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed )
+            {
+                window.close();
+            }
+        }
+
+
+        if(window.hasFocus())
+        {
+            window.setSize({ 800, 600 });
+            window.setTitle("Focus");
+
+        }else{
+            window.setSize({ 400, 300 });
+            window.setTitle("No Focus");
+
+            if(onetime)
+            {
+                onetime = false;
+
+                sf::Window noFocus;
+                std::thread nfT{ noFocusThread, std::ref(noFocus) };
+                nfT.detach();
+
+            }
+        }
+
+        window.clear();
+        window.display();
+
+
+    }
+
+
+    return 0;
 }
