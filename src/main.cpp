6892c2c069e90d66329ab106a3a662c905f41479
nicolas <nicolasantiagomez4@gmail.com>
2023-12-17 19:23:38 -0300

    ab

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,14 @@
 #include <SFML/Graphics.hpp>
-#include <thread>
+#include <iostream>
 
-
-void noFocusThread(sf::Window& window)
-{
-
-    window.create({ 400, 300 }, "No Focus", sf::Style::Default);
-
-    while(window.isOpen())
-    {
-        sf::Event event;
-
-        while(window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-
-        window.display();
-    }
-}
 
 
 int main()
 {
-
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
 
     auto window = sf::RenderWindow{ { desktop.width, desktop.height }, "titulo", sf::Style::Default };
     window.setFramerateLimit(144);
-
-
-
-    bool onetime = true;
 
     while (window.isOpen())
     {
@@ -51,29 +24,8 @@ int main()
         }
 
 
-        if(window.hasFocus())
-        {
-            window.setSize({ 800, 600 });
-            window.setTitle("Focus");
-
-        }else{
-            window.setSize({ 400, 300 });
-            window.setTitle("No Focus");
-
-            if(onetime)
-            {
-                onetime = false;
-
-                sf::Window noFocus;
-                std::thread nfT{ noFocusThread, std::ref(noFocus) };
-                nfT.detach();
-
-            }
-        }
-
         window.clear();
         window.display();
-
 
     }
 
